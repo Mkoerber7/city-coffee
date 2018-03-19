49ffa0c8dec199ab6a3fe209ece0fa15a04a051a
Michael <Mkoerber7@gmail.com>
2018-03-19 07:59:48 -0500

    Cleaned up a few styles, added contant picture,

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import spSketch from "../assets/spSketch.jpg"
 
 const Contact = () => {
         return (
           <div className="contact-container">
             <h1 className="contact-title">Contact</h1>
-            <p className="info">Come see us at 135 Los Gatos Road, Arlen, Texas <br/> or give us a call at 123--456-7899 </p>
+            <p className="info">Come see us at 135 Los Gatos Road, Arlen, Texas <br/> or give us a call at 123-456-7899. </p>
+            <img className="sketch" src={spSketch} alt="Strickland Propane"/>
           </div>
         )
 }
