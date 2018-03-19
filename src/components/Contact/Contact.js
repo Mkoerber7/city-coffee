4f0baaf7eea02752061832310872c2cd9c179962
Michael <Mkoerber7@gmail.com>
2018-03-19 00:03:27 -0500

    added content to contact page and fixed styling with product page images

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Contact = () => {
         return (
           <div className="contact-container">
-          <h1>Contact</h1>
+            <h1 className="contact-title">Contact</h1>
+            <p className="info">Come see us at 135 Los Gatos Road, Arlen, Texas <br/> or give us a call at 123--456-7899 </p>
           </div>
         )
 }
